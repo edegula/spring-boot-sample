f730e669cf9bbc49c65705eb20f9fd469a07a0ef
Erwin Dg <tigastitig@gmail.com>
2020-06-05 20:53:02 +1000

    uses spring-data-jpa to persist courses

--- src/main/java/com/example/springboot/topic/Topic.java
+++ src/main/java/com/example/springboot/topic/Topic.java
@@ -1,20 +1,27 @@
 package com.example.springboot.topic;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Getter
 @Setter
-@NoArgsConstructor
+@Entity
 public class Topic {
 
+    @Id
     private String id;
     private String name;
     private String description;
 
+    public Topic() {
+
+    }
+
     public Topic(String id, String name, String description) {
+        super();
         this.id = id;
         this.name = name;
         this.description = description;
@@ -22,5 +29,25 @@ public class Topic {
 
     public String getId() {
         return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 }
